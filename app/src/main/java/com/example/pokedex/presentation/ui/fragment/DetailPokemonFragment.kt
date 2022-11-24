3f66e926e081acaf071d4975950d36b2f9203965
bayazidsustami <bayazidsustamy@gmail.com>
2022-11-25 01:30:10 +0800

    feat : set colors of details

--- app/src/main/java/com/example/pokedex/presentation/ui/fragment/DetailPokemonFragment.kt
+++ app/src/main/java/com/example/pokedex/presentation/ui/fragment/DetailPokemonFragment.kt
@@ -3,8 +3,11 @@ package com.example.pokedex.presentation.ui.fragment
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.pokedex.data.datasource.local.entity.PokemonEntity
 import com.example.pokedex.databinding.FragmentDetailBinding
+import com.example.pokedex.presentation.getColorRes
 import com.example.pokedex.presentation.loadImage
 import com.example.pokedex.presentation.ui.base.BaseFragment
 
@@ -23,12 +26,32 @@ class DetailPokemonFragment: BaseFragment<FragmentDetailBinding>(FragmentDetailB
                 tvPokeName.text = it.pokeName
                 tvPokeNumber.text = it.pokeNumber
             }
+
+            setColors(it.colorTypes)
         }
 
         binding.ivBack.setOnClickListener {
             activity?.finish()
         }
 
+    }
+
+    private fun setColors(colorTypes: String) {
+        with(binding) {
+            container.setBackgroundResource(getColorRes(colorTypes))
+            tvAbout.setTextColor(colorTypes)
+            tvBaseStats.setTextColor(colorTypes)
+            viewStats.tvHp.setTextColor(colorTypes)
+            viewStats.tvAtk.setTextColor(colorTypes)
+            viewStats.tvDef.setTextColor(colorTypes)
+            viewStats.tvSatk.setTextColor(colorTypes)
+            viewStats.tvSdef.setTextColor(colorTypes)
+            viewStats.tvSpd.setTextColor(colorTypes)
+        }
+    }
+
+    private fun TextView.setTextColor(colorTypes: String) {
+        setTextColor(ContextCompat.getColor(requireContext(), getColorRes(colorTypes)))
     }
 
     override fun onDestroyView() {
